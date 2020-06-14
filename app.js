489bd6d1f92170e3467e9450dca1b4b6a3e67d8c
Sohaghori839 <56892915+Sohaghori839@users.noreply.github.com>
2020-06-14 04:54:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,369 @@
+//Task 1
+//alert("Hello World")
+
+//Task 2
+//alert("Error! Please Enter a Valid Password")
+
+//Task 3
+//alert("welcome to js... \n Happy coding")
+
+
+//Task 4
+//alert("Welcome to js..")
+//alert("Happy Coding! \n Preevent this page from creating aditional dialogs.")
+
+//Task 5
+//console.log("Hello... i can run js through my web browser's console")
+
+//CHAPTER 3
+//task 1
+//var username;
+
+//task 2
+//var myName ="soha ghori";
+
+//task 3
+//var massage;
+//massage="Hello world";
+//alert(massage);
+
+//task 4
+//alert("Jhoun Doe");
+//alert("15 years old");
+//alert("Certified Mobile Application Development");
+
+//task 5
+//var a="Pizza";
+//alert(a, "\n", a[2]);
+
+//task 6
+//var email="Sohaghori839@gmail.com";
+//alert("My email address is " + email);
+
+//task 7
+//var book="A smarter way to learn JavaScript";
+//alert("I am trying to Learn from the Book " + book);
+
+//task 8
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//task 9
+//alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+//document.write("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+
+//Chapter 3
+//task 1
+//var age="19";
+//alert("I am "+age+" years old");
+
+//task 2
+//var birthYear="2000";
+//document.write("My birth year is "+ birthYear+ "<br>" );
+//document.write("Data type of my declared variable is Number"); 
+
+//task 3
+//var VisitorName=prompt("Enter Your name");
+//var Product=prompt("Enter Your Product");
+//document.write(VisitorName +" ordered " + Product + " on XYZ Clothing store")
+
+//Chapter 4 
+//task 1 
+//var a,b,c;
+
+//task 2
+//Legal
+//var a1,abc54,first_name,$pqr,_z;
+//illegal
+//var 8a,&ut,:qa,#z,@p;
+
+//task 3
+ //document.write("Rules for naming JS variables "+"<br>"+"<br>"+"<br>");
+ //document.write("Variable names can only contain numbers, _, and $. For example $my_1stVariable" +"<br>")
+ //document.write("Variables must begin with a letter, $ or _. For example $name, _name or name"+"<br>")
+ //document.write("Variable names are case sensitive"+"<br>");
+ //document.write ("Variable names should not be JS keywords");
+
+ //Chapter 5
+//var val1=prompt("enter first number");
+//var sign=prompt("enter your sign");
+//var val2=prompt("enter last number");
+//console.log(val1+sign+val2);
+//if(sign === '+'){
+//     alert(val1+val2)
+// }else if(sign === '-'){
+ //   alert(val1+val2)
+//}else if(sign === '*'){
+  //  alert(val1+val2)
+//}else if(sign === '/'){
+  //  alert(val1+val2)
+//}
+
+// task 5
+//for(var i=1; i<=10; i++){
+ //   document.write("4" + "x" + i + "="+ 4*1 + "</br>");
+//}
+
+//task 6
+//var c = 25;
+//var convertf = (c*9/5)+32;
+//var f = 70;
+//var convertc = (f-32)*5/9;
+//document.write("25°C is "+convertf+"°F"+"<br>"+"70°F is "+convertc+"°c");
+
+//task 3
+// a part
+//var a ="??";
+//b part
+//document.write("value after variable declaration is: "+a +"<br>");
+//c part
+//var a = 5 ;
+//document.write("initial value is: "+a+ "<br>");
+//d part
+//var b = ++a;
+//f part
+//document.write("value after increment is: "+b+ "<br>");
+//g part
+//var b = b+7;
+//h part
+//document.write("value after addition is: "+b +"<br>");
+//i part
+//var c= --b;
+//j part
+//document.write("value after decrement is: "+c+ "<br>");
+//k part
+//var d = 0/3;
+//L part
+//document.write("The remainder is "+d);
+
+
+//Chapter 6 7 8 9 10
+//task 1
+//var a = 10;
+//var a = ++a;
+//var a = a++;
+//var a = --a;
+//var a = a--;
+//document.write("Result:"+ "<br>" + "The value of a is: 10" + "<br>" + "......................................." + 
+//"<br>" + "The value of ++a is: 11" + "<br>" + "Now the value of a is: 11" + "<br><br><br>" + "The value of a++ is: 11" +
+//"<br>" + "Now the value of a is: 12" + "<br><br><br>" + "The value of --a is: 11" +
+//"<br>" + "Now the value of a is: 11" + "<br><br><br>" + "The value of a-- is: 11" +
+//"<br>" + "Now the value of a is: 10" + "<br><br><br>" );
+
+//task 2
+//var a = 2;
+//var b = 1;
+//var r =  --a - --b + ++b + b-- ;
+//document.write("--a = 1" +"<br>"+ "--a - --b = 1" +"<br>"+ " --a - --b + ++b = 2" +"<br>"+ "--a - --b + ++b + b-- = 3"
+//+ "<br><br>" + "a is "+a + "<br>" + "b is "+b+ "<br>"+"result is "+r); 
+
+//task 3
+//var a = "hello";
+//var b = "Enter your name"
+//var c = prompt(a , b);
+//alert(c+" welcome to this website"); 
+
+//task 4
+
+//task 5
+//var a = +prompt("Enter your number");
+//if(a ==""){
+//    for (var i =1 ; i<=10; i++){
+//        document.write(5*i + "<br>");
+//}}
+//else{
+//for (var i =1 ; i<=10; i++){
+ //   document.write(a*i + "<br>");
+//}
+//}
+
+//task 6
+//var sub1= prompt("Enter your first subject name","");
+//var sub2= prompt("Enter your second subject name","");
+//var sub3= prompt("Enter your third subject name","");
+//var entersub1 = sub1;
+//var entersub2 = sub2;
+//var entersub3 = sub3;
+//var totalmarks = 100;
+//var alltotalmarks = totalmarks*3;
+//var obtainmarks1 = +prompt("enter your first subject marks");
+//var obtainmarks2 = +prompt("enter your second subject marks");
+//var obtainmarks3 = +prompt("enter your third subject marks");
+//var eobtainmarks1 = obtainmarks1;
+//var eobtainmarks2 = obtainmarks2;
+//var eobtainmarks3 = obtainmarks3;
+//var totalobtain = obtainmarks1+obtainmarks1+obtainmarks1;
+//var percentage1 = (obtainmarks1/totalmarks)*100;
+//var percentage2 = (obtainmarks2/totalmarks)*100;
+//var percentage3 = (obtainmarks3/totalmarks)*100;
+//var totalpercentage =(totalobtain/alltotalmarks)*100;
+//document.write("Subject "+ "<br>" + sub1+"<br>"+sub2+"<br>"+sub3+"<br>");
+//document.write("Total Marks"+"<br>"+totalmarks+"<br>"+totalmarks+"<br>"+totalmarks+"<br>"+totalmarks+"<br>"+alltotalmarks+"<br>");
+//document.write("Obtain Marks"+"<br>"+eobtainmarks1+"<br>"+eobtainmarks2+"<br>"+eobtainmarks3+"<br>"+totalobtain+"<br>");
+//document.write("Percentage"+"<br>"+percentage1+"%"+"<br>"+percentage2+"%"+"<br>"+percentage3+"%"+"<br>"+totalpercentage+"%"+"<br>");
+
+// Task no.2
+// var a=+prompt("Eneter any number ")
+// var b=+prompt("Eneter any number ")
+// if (a===b){
+//     alert("Numbers are equal")
+// }
+// else{
+//     alert("Numbers are not equal")
+// }
+
+
+// Task no.3
+// var a=+prompt("Eneter any number ")
+// if(a>0){
+//     alert('Number is Positive')
+// }
+// else if(a<0){
+//     alert('Number is Negative')
+// }
+// else if(a===0){
+//     alert('Number is Zero')
+// }
+
+
+// Task no.4
+// var x=prompt("Eneter any alphabet ")
+// if(x==="a" || x==="e" || x==="i" || x==="o" || x==="u"){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+
+// Task no.5
+// var password="1234"
+// var pass=prompt("Please Enter your password ")
+
+// if(pass===password){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Password is Incorrect")
+// }
+
+
+
+// Task no.6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// alert(greeting)
+// }
+// else{
+// greeting = "Good evening"
+// alert(greeting)
+// }
+
+
+// Task no.7
+// var a=prompt("Enter any number from 24 hours clock ")
+// if(a==0){
+//     alert("0000=12am")
+// }
+// else if(a==1){
+//     alert("0100=1am")
+// }
+// else if(a==2){
+//     alert("0200=2am")
+// }
+// else if(a==3){
+//     alert("0300=3am")
+// }
+// else if(a==4){
+//     alert("0400=4am")
+// }
+// else if(a==5){
+//     alert("0500=5am")
+// }
+// else if(a==6){
+//     alert("0600=6am")
+// }
+// else if(a==7){
+//     alert("0700=7am")
+// }
+// else if(a==8){
+//     alert("0800=8am")
+// }
+// else if(a==9){
+//     alert("0900=9am")
+// }
+// else if(a==10){
+//     alert("1000=10am")
+// }else if(a==11){
+//     alert("1100=11am")
+// }else if(a==12){
+//     alert("1200=12pm")
+// }else if(a==13){
+//     alert("1300=1pm")
+// }
+// else if(a==14){
+//     alert("1400=2pm")
+// }
+// else if(a==15){
+//     alert("1500=3pm")
+// }
+// else if(a==16){
+//     alert("1600=4pm")
+// }
+
+// else if(a==17){
+//     alert("1700=5pm")
+// }
+
+// else if(a==18){
+//     alert("1800=6pm")
+// }
+
+// else if(a==19){
+//     alert("1900=7pm")
+// }
+// else if(a==20){
+//     alert("2000=8pm")
+// }
+// else if(a==21){
+//     alert("2100=9pm")
+// }
+// else if(a==22){
+//     alert("2200=10pm")
+// }
+// else if(a==23){
+//     alert("2300=11pm")
+// }
+//chapter 14
+//task 15
+//var arr=["Apple", "Samsung", "Motorola", "Nokia", "Sony & Haier"]
+//document.write("<select>")
+//for(var i=0; i< arr.lengt; i++){
+//document.write("<option>"+ arr[i]+ "</option>")
+//}
+//document.write("</select>")
+   
+//chapter 14
+// task 1
+//var student =[];
+//student[0] = "soha";
+//alert(student)
+
+//task 2
+//var coures =["developer", "programmer"];
+//var coures= new course();
+//alert(course);
+
+//chapter 17 
+//task 5
+//var items= ["cack", "apple pie", "cookie", "chips", "patties"];
+//var search = prompt("enter seach value");
+//if(items.indexOf(search) !== -1){
+ //   alert("yes"  + search + "is available")
+//} else{
+ //   alert("No" + search + "is not available")
+//}
+
+
+
